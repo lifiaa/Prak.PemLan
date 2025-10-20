c95bc37f3e0cb4162d110e5d0514c03379eb779d
Lifiaa <lifidianto@gmail.com>
2025-10-20 10:36:17 +0700

    2nd Commit

--- src/Modul3/Main.java
+++ src/Modul3/Main.java
@@ -3,5 +3,6 @@ package Modul3;
 public class Main {
     public static void main(String[] args){
         System.out.println("Halo, ini adalah Git Integrated Java Project Pertamaku!");
+        System.out.println("Halo, ini adalah Git Integrated Java Project Keduaku!");
     }
 }
